963d61f2856aa02d0c67060dcd7116cce8795b87
World Plus <worldplus.dev@gmail.com>
2019-08-10 17:15:58 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-import './index.css';
+import './index.css'
 import reducer from './reducers'
-import EventsIndex from './components/events_index';
-import * as serviceWorker from './serviceWorker';
+import EventsIndex from './components/events_index'
+import EventsNew from './components/events_new'
+import * as serviceWorker from './serviceWorker'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <Provider store={store}>
-        <EventsIndex />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
